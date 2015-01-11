d3b5ddb2c2e4d8988d351220528271a12958f9a3
Daniel A. Weiner <DanielW@Daniels-MacBook-Pro.local>
2015-01-11 15:31:30 -0500

    Initial Commit

---
+++ emperors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  emperors
+//
+//  Created by Daniel A. Weiner on 1/11/15.
+//  Copyright (c) 2015 Daniel Weiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ emperors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  emperors
+//
+//  Created by Daniel A. Weiner on 1/11/15.
+//  Copyright (c) 2015 Daniel Weiner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
